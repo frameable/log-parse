2a72d6b73b08a762575afdbb53d037af8548c3bb
Eva <mail@gastrodon.io>
2023-08-03 18:06:48 -0400

    make uniqe /tmp directories by default

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import { mkdtempSync } from "node:fs";
+import { join } from "node:path";
+import { tmpdir } from "node:os";
 import { Context } from "./model";
 
 export { Context, ColumnIndex, LogLine, Entry, EntryIterFunc, EntryIter } from "./model"
@@ -7,10 +10,10 @@ export { iterLogs } from "./src/read-file"
 export const ctx = (part?: Partial<Context>): Context => ({
   entryFields: new Set<string>(),
   sqliteTable: "logs",
-  sqliteRoot: "/tmp",
+  sqliteRoot: mkdtempSync(join(tmpdir(), "")),
   sqliteInMemory: false,
   sqliteColumnIndexes: [],
-  logsRoot: "/tmp",
+  logsRoot: mkdtempSync(join(tmpdir(), "")),
   logsDaysAgo: 1,
   ...part,
 })
